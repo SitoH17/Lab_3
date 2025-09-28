2ad8c7ab4580408307f3fc4a58aee0a6f0095ddf
SitoH17 <randallalonso17@gmail.com>
2025-09-27 21:05:10 -0600

    Plantilla del ejercicio2

---
+++ ejercicio2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SIZE 5
+int findLargestLine(int matrix[][SIZE] {
+// implementacion
+
+}
+
+int main () {
+int matrix[SIZE][SIZE] = {
+{2,7,6},
+{9,5,1},
+{4,3,8}
+};
+
+int largestLine == findLargestLine (matrix);
+printf("El tama;o de la secuencia de ls mas grande es: %d\n" , largestLine)
+;
+return 0;
+}
+
